8877629d14f5047cdbf99f35c8c61c4678e713d7
yusuf <horiyorrmi72@gmail.com>
2025-05-11 02:54:12 +0100

    feat(applicants):add applicant route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import userRoute from '../controllers/users/user.route'
 import authRoute from '../controllers/auth/auth.route'
 import jobRoute from '../controllers/job/job.route'
+import applicantRoute from '../controllers/applicants/applicant.route'
 const router = express.Router()
 
 router.use('/users', userRoute);
 router.use('/auth', authRoute);
-router.use('/jobs', jobRoute)
+router.use('/jobs', jobRoute);
+router.use('/applicant', applicantRoute)
 
 export default router;
