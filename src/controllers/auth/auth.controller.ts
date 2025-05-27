f9b8bcc0d09252c998a835359318d225465ba6b5
yusuf <horiyorrmi72@gmail.com>
2025-05-27 05:40:56 +0100

    feat(active users):add users to active users upon login

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,29 +1,38 @@
-import { signUpValidation, signInValidation } from '../../utils/validators/auth.validators';
+import { signUpValidation, signInValidation, forgotPasswordValidator, resetPasswordValidator } from '../../utils/validators/auth.validators';
 import AuthService from '../../services/auth/auth.service';
 import { Request, Response, NextFunction } from 'express';
-import { BadRequest } from '../../utils/errors';
-import { generateToken } from '../../utils/auth/auth.utils';
-
+import { BadRequest, UnauthorizedError } from '../../utils/errors';
+import { decodeToken, generateSessionId, generateToken } from '../../utils/auth/auth.utils';
+import Mailer from '../../utils/others/mailer';
+import logger from '../../utils/logger';
+import mongoose, { ObjectId } from 'mongoose';
+import { Types } from 'joi';
+import PublicService from '../../services/others/public.service';
 
 class AuthController {
   static async signup(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       let activationLink;
       const { error } = signUpValidation(req.body);
+      const { role } = req.query;
       if (error) {
         throw new BadRequest(`${error.details[0].message}`);
         return;
       }
-      const user = await AuthService.register(req.body);
-      // send activation link to mail if user is an applicant
-      // generate activation link
-      if (user.role === 'applicant') {
-        let activationToken = await AuthService.generateActivationLink(user._id as string);
-        activationLink = `${req.protocol}://${req.get('host')}/api/auth/activate/${activationToken}`;
-        // console.log('token', user)
+      if (role === 'admin') {
+        logger.error(`${req.ip} tried creating an admin!`);
+        throw new UnauthorizedError(`you do not have the permission to perform this action, kindly contact the system administrator!`);
+        return;
       }
-      // console.log('link', activationLink)
-      res.status(201).json({ success: true, data: user, activationLink})
+      const payload = { ...req.body, role }
+      const user = await AuthService.register(payload);
+      // send activation link to mail if user is an applicant
+      if (user.role === 'applicant') {
+        let activationToken = await AuthService.generateActivationLink(user._id as ObjectId);
+        activationLink = `${req.protocol}://${req.get('host')}/api/auth/activate/${activationToken}`;
+        // await Mailer.sendActivationMessage(user.email, `Applicant Account Activation`, activationLink);
+      }
+      res.status(201).json({ success: true, data: user, activationLink })
       return;
 
     } catch (error) {
@@ -35,7 +44,7 @@ class AuthController {
   static async activateAccount(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const { activationToken } = req.params;
-      
+
       const tokenController = await AuthService.activateAccount(activationToken);
       // console.log('tokenController', tokenController)
       res.status(200).json({ message: 'Account activated successfully' });
@@ -49,18 +58,100 @@ class AuthController {
   static async signIn(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const { error } = signInValidation(req.body);
+
       if (error) {
         throw new BadRequest(`${error.details[0].message}`);
       }
 
       const user = await AuthService.login(req.body);
-      const { _id, role } = user as { _id: string; role: string };
-      const token = await generateToken(_id, role);
-      res.status(200).json({ success: true, data: user, token });
+      let { _id, role, preferred_name, name } = user as { _id: mongoose.Schema.Types.ObjectId; role: string, preferred_name: string, name: string };
+      let idStr = _id.toString();
+      const token = await generateToken(idStr, role);
+      const tokenExp = await decodeToken(token);
+      const sessionId = await generateSessionId(tokenExp as number);
+
+      res.cookie('auth_token', token, {
+        httpOnly: true,
+        // secure: process.env.NODE_ENV === 'production',
+        secure: true,
+        sameSite: 'none',
+        maxAge: 1000 * 60 * 60,
+      });
+      res.cookie('sessionId', sessionId, {
+        httpOnly: true,
+        // secure: process.env.NODE_ENV === 'production',
+        secure: true,
+        sameSite: 'none',
+        maxAge: (tokenExp as number * 100) - Date.now(),
+      });
+      let userData = { preferred_name, name, role, idStr };
+      const resPayload = { userData };
+      await PublicService.addToActiveUsersList(sessionId.generatedId, userData, tokenExp as number);
+      res.status(200).json({ success: true, message: 'Login Successfully', data: resPayload });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+
+  static async resendActivationLink(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      await AuthService.resendActivationLink(req);
+      res.status(200).json({ success: true, message: 'activation link sent to unactivated accounts' });
+
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async forgotPassword(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { error } = forgotPasswordValidator(req.body);
+      if (error) {
+        logger.error(`Error at forgot password: ${error.details[0].message}`);
+        throw new BadRequest(`Error at forgot password: ${error.details[0].message}`)
+      }
+      await AuthService.forgotPassword(req.body, req);
+      res.status(200).json({ success: true, message: 'Reset link sent to your mail' });
+      return;
+    } catch (error) {
+      next(error);
+
+    }
+  }
+  static async resetPassword(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { error } = resetPasswordValidator(req.body);
+      if (error) {
+        throw new BadRequest(`Error at reset password: ${error.details[0].message}`)
+        return;
+      }
+      await AuthService.resetPassword(req.body);
+      res.status(200).json({ success: true, message: 'Password reset successfully' });
+      return;
+
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getCurrentUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const token = req.cookies.auth_token;
+      if (!token) {
+        throw new UnauthorizedError('Unauthorized, you do not have the permission to perform this action');
+      }
+      const user = await AuthService.getUserFromToken(token);
+      if (!user) {
+        throw new UnauthorizedError('user not found or invalid token');
+      }
+      res.json({ user });
+      return;
+
     } catch (error) {
       next(error);
     }
   }
 }
+
 
 export default AuthController
--- src/controllers/auth/auth.route.ts
+++ src/controllers/auth/auth.route.ts
@@ -1,12 +1,20 @@
 import express from 'express'
 import AuthController from './auth.controller'
+import passport from '../../middlewares/passport.middleware';
 
 
 const router = express.Router()
 
+
+
+
+router.get('/me', AuthController.getCurrentUser);
 router.post('/signup', AuthController.signup)
 router.post('/signin', AuthController.signIn)
 router.get('/activate/:activationToken', AuthController.activateAccount)
+router.post('/resend-activation', AuthController.resendActivationLink);
+router.post('/forgot-password', AuthController.forgotPassword);
+router.post('/reset-password', AuthController.resetPassword)
 
 
 export default router
