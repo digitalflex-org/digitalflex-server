a21d945733b0f27df018d88cbc21d5fdb5c56456
yusuf <horiyorrmi72@gmail.com>
2025-07-04 15:22:14 +0100

    feat(middlewares):add middlewares

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -3,43 +3,43 @@ import { rolesHierarchy } from '../models/user.model';
 import { UnauthorizedError } from '../utils/errors';
 
 export const permit = (...allowedPermissions: string[]) => {
-    return (req: Request, res: Response, next: NextFunction): void => {
-        const userRole = req.user?.role as keyof typeof rolesHierarchy | undefined;
+  return (req: Request, res: Response, next: NextFunction): void => {
+    const userRole = req.user?.role as keyof typeof rolesHierarchy | undefined;
 
-        if (!userRole) {
-            res.status(401).json({ sucess: false, message: "Unauthorized, you are not authorized to perform this action!" });
-            return;
-        }
-        const userPermissions = rolesHierarchy[userRole];
-        const hasPermission = allowedPermissions.some(permission => userPermissions.includes(permission));
+    if (!userRole) {
+      res.status(401).json({ sucess: false, message: 'Unauthorized, you are not authorized to perform this action!' });
+      return;
+    }
+    const userPermissions = rolesHierarchy[userRole];
+    const hasPermission = allowedPermissions.some(permission => userPermissions.includes(permission));
 
-        if (!hasPermission) {
-            res.status(403).json({ message: "Forbidden, you do not have the permission to perform this action!" });
-            return;
-        }
+    if (!hasPermission) {
+      res.status(403).json({ message: 'Forbidden, you do not have the permission to perform this action!' });
+      return;
+    }
 
-        next();
-    };
+    next();
+  };
 };
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction): void => {
-    const userRole = req.user?.role as keyof typeof rolesHierarchy;
+  const userRole = req.user?.role as keyof typeof rolesHierarchy;
 
-    if (userRole === 'admin') {
-        next();
-    } else {
-        next(new UnauthorizedError('Unauthorized, you do not have permission to perform this action'));
-    }
+  if (userRole === 'admin') {
+    next();
+  } else {
+    return next(new UnauthorizedError('Unauthorized, you do not have permission to perform this action'));
+  }
 };
 
 export const requiredRoles = (roles: (keyof typeof rolesHierarchy)[]) => {
-    return (req: Request, res: Response, next: NextFunction): void => {
-        const userRole = req.user?.role as keyof typeof rolesHierarchy | undefined;
+  return (req: Request, res: Response, next: NextFunction): void => {
+    const userRole = req.user?.role as keyof typeof rolesHierarchy | undefined;
 
-        if (!userRole || !roles.includes(userRole)) {
-            return next(new UnauthorizedError('Unauthorized, you do not have permission to perform this action'));
-        }
+    if (!userRole || !roles.includes(userRole)) {
+      return next(new UnauthorizedError('Unauthorized, you do not have permission to perform this action'));
+    }
 
-        next();
-    };
+    next();
+  };
 };
