9879854ab5014ed07e8ea772ece6727cadff2be4
yusuf <horiyorrmi72@gmail.com>
2025-07-04 15:01:37 +0100

    feat(update and deletion of posted jobs):add update and deletion feature for jobs

--- src/controllers/job/job.controller.ts
+++ src/controllers/job/job.controller.ts
@@ -10,89 +10,91 @@ import Job, { jobInterface } from '../../models/job.model';
 
 
 class JobController {
-    static async getAllJobs(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-            const jobs = await JobService.getJobs();
-            if (!jobs || jobs.length === 0) {
-                throw new NotFoundError('No jobs at the moment check back later!')
-            }
-
-            res.status(200).json({ success: true, jobs })
-            return;
-        } catch (error) {
-            next(error)
-        }
+  static async getAllJobs(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { limit = '10', page = '1' } = req.query;
+      const parseLimitToNumber = parseInt(limit as string, 10);
+      const parsePageToNumber = parseInt(page as string, 10);
+      const jobs = await JobService.getJobs(parseLimitToNumber, parsePageToNumber);
+      if (!jobs || jobs.length === 0) {
+        throw new NotFoundError('No jobs at the moment check back later!')
+      }
+      res.status(200).json({ success: true, jobs })
+      return;
+    } catch (error) {
+      next(error)
     }
+  }
 
-    static async getJob(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-            let job;
-            const { identifier } = req.params;
-            if (!identifier) {
-                throw new BadRequest('verify selected job exists')
-            }
-            // console.log(identifier);
-            job = isValidObjectId(identifier) ? await JobService.getJobById(identifier) : await JobService.getJobBySlug(identifier)
-            if (!job) throw new NotFoundError('Job Not Found!');
-            res.status(200).json({ 'success': true, job })
-            return;
-        } catch (error) {
-            next(error)
-        }
+  static async getJob(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      let job;
+      const { identifier } = req.params;
+      if (!identifier) {
+        throw new BadRequest('verify selected job exists')
+      }
+      // console.log(identifier);
+      job = isValidObjectId(identifier) ? await JobService.getJobById(identifier) : await JobService.getJobBySlug(identifier)
+      if (!job) throw new NotFoundError('Job Not Found!');
+      res.status(200).json({ 'success': true, job })
+      return;
+    } catch (error) {
+      next(error)
     }
+  }
 
-    static async addJob(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-            const { error } = createJobValidation(req.body);
-            if (error) {
-                throw new BadRequest(`"missing required parameters!:"
+  static async addJob(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { error } = createJobValidation(req.body);
+      if (error) {
+        throw new BadRequest(`"missing required parameters!:"
                 ${error.message}`)
-            }
-            const userObj = Object(req.user);
-            const postedBy = userObj.id;
-            // console.log(postedBy)
-            const payload = { postedBy, ...req.body }
-            const job = await JobService.createJob(payload);
-            res.status(201).json({ success: true, job })
-            return;
-        } catch (error) {
-            next(error)
+      }
+      const userObj = Object(req.user);
+      const postedBy = userObj.id;
+      // console.log(postedBy)
+      const payload = { postedBy, ...req.body }
+      const job = await JobService.createJob(payload);
+      res.status(201).json({ success: true, job })
+      return;
+    } catch (error) {
+      next(error)
 
-        }
     }
+  }
 
-    static async updateJob(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-            const { id } = req.params;
-            const { deadline, ...data } = req.body;
-            const user = Object(req.user);
-            const updatedBy = user.id;
-            const payload = {
-                data,
-                updatedBy
-            }
+  static async updateJob(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { id } = req.params;
+      const { deadline, ...data } = req.body;
+      const user = Object(req.user);
+      const updatedBy = user.id;
+      const payload = {
+        ...data,
+        updatedBy
+      }
 
-            const updatedJob = await JobService.updateJobDetails(id, payload);
-            res.status(200).json({ success: true, message: 'Job details updated sucessfully!', job: updatedJob });
-        } catch (error) {
-            next(error);
-        }
+      const updatedJob = await JobService.updateJobDetails(id, payload);
+      res.status(200).json({ success: true, message: 'Job details updated sucessfully!', job: updatedJob });
+    } catch (error) {
+      next(error);
     }
+  }
 
 
-    static async deleteJob(req: Request, res: Response, next: NextFunction) {
-        try {
-            const { ids } = req.body
-            if (!ids || !Array.isArray(ids)) {
-                throw new BadRequest(`Not a valid format for items to be deleted. id's must be an array of string`)
-            }
-            await JobService.deleteJobs(ids as string[])
-            res.status(204).send();
-            // return;
-        } catch (error) {
-            next(error);
-        }
+  static async deleteJob(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { ids } = req.body
+      if (!ids || !Array.isArray(ids)) {
+        throw new BadRequest('Not a valid format for items to be deleted. id\'s must be an array of string')
+      }
+      await JobService.deleteJobs(ids as string[])
+      res.status(204).send();
+      // return;
+    } catch (error) {
+      next(error);
     }
+  }
 }
 
 export default JobController
