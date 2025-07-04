f51f9c07c8224e247c089853638f0e1e24b92020
yusuf <horiyorrmi72@gmail.com>
2025-07-04 15:09:26 +0100

    chore(move db connection to entry):move database connections to the entry file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { configVariables } from './config/envConfig';
-import connectDb from './config/dbConfig';
+import { connectDb } from './config/dbConfig';
 import { connectRedis } from './config/redisConfig';
 // import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser';
@@ -12,27 +12,26 @@ import passport from './middlewares/passport.middleware'
 const app = express()
 
 
-connectDb()
-connectRedis()
+// connectDb()
+// connectRedis()
 
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://c48e-102-88-111-153.ngrok-free.app',
-    '102.88.111.153',
-  ];
+  'http://localhost:3000',
+  '102.88.111.153',
+];
 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        }
-        callback(new Error('Not allowed by CORS'));
-    },
-    credentials: true,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    callback(new Error('Not allowed by CORS'));
+  },
+  credentials: true,
 }));
 app.use(passport.initialize())
 
