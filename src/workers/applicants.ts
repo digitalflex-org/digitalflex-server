9698e6edf78c3d76ceb887c39b8d557c1498e0aa
yusuf <horiyorrmi72@gmail.com>
2025-07-04 15:31:11 +0100

    feat(applicant worker):add applicant worker

--- src/workers/applicants.ts
+++ src/workers/applicants.ts
@@ -1,23 +1,33 @@
-import ApplicantService from "../services/apllicant/applicant.service";
-import ApplicantController from "../controllers/applicants/applicant.controller";
-import { BaseError } from "../utils/errors/BaseError";
-import logger from "../utils/logger";
+import ApplicantService from '../services/apllicant/applicant.service';
+import { BaseError } from '../utils/errors/BaseError';
+import logger from '../utils/logger';
 
-class ApplicantWorker{
-    static async notActivatedChecker() {
-        try {
-            
-        } catch (error) {
-            if (error instanceof BaseError) {
-                logger.error('Unable to check for unactivated applicant from W', error.message, error.isOperational=false);
-            } else {
-                logger.error('Unknown Error', error)
-            }
-            throw error;
-            
-        }
+class ApplicantWorker {
+  static async notActivatedChecker() {
+    try {
+      await ApplicantService.retryNotActivatedApplicants();
+    } catch (error) {
+      if (error instanceof BaseError) {
+        logger.error('Unable to check for unactivated applicants from worker', error.message);
+      } else {
+        logger.error('Unknown error in notActivatedChecker', error);
+      }
+      throw error;
     }
+  }
 
+  static async inactiveChecker() {
+    try {
+      await ApplicantService.pingApplicantsLastActive();
+    } catch (error) {
+      if (error instanceof BaseError) {
+        logger.error('Unable to check for inactive applicants', error.message);
+      } else {
+        logger.error('Unknown error in inactiveChecker', error);
+      }
+      throw error;
+    }
+  }
 
 }
 
